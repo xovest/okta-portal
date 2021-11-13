4061314c3715cd8bb281e9a0e02f265cba3fc504
xovest <79008917+xovest@users.noreply.github.com>
2021-11-13 20:34:48 +0200

    implemented everything from okta in here

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
+import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import Staff from './components/pages/Staff';
+import Login from './components/auth/Login';
+import Protected from './components/auth/Protected';
+import { oktaAuthConfig, oktaSignInConfig } from './config';
+
+const oktaAuth = new OktaAuth(oktaAuthConfig);
 
 function App() {
+  const navigate = useNavigate();
+
+  const customAuthHandler = () => {
+    navigate('/login');
+  };
+
+  const restoreOriginalUri = async (_oktaAuth, originalUri) => {
+    navigate(toRelativeUrl(originalUri, window.location.origin), { replace: true });
+  };
+
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-        
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/staff" element={<Staff />} />
-        </Routes>
-      </div>
+      <Security
+        oktaAuth={oktaAuth}
+        onAuthRequired={customAuthHandler}
+        restoreOriginalUri={restoreOriginalUri}
+      >
+        <div className="App">
+          <Navbar />
+          
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/staff" element={<Staff />} />
+            <SecureRoute path='/protected' component={Protected} />
+            <Route path='/login' render={() => <Login config={oktaSignInConfig} />} />
+            <Route path='/login/callback' component={LoginCallback} />
+          </Routes>
+        </div>
+      </Security>
     </Router>
   );
 }
